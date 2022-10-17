24a2d47caa25c543bff436ab9e13eae314e3d15b
Patrick <g00409592@atu.ie>
2022-10-17 21:27:44 +0100

    trying to fix things

--- src/main/java/atu/ie/week5/ShapeApp.java
+++ src/main/java/atu/ie/week5/ShapeApp.java
@@ -1,5 +1,6 @@
 package atu.ie.week5;
 
+import java.awt.*;
 import java.util.Scanner;
 
 public class ShapeApp {
@@ -9,7 +10,9 @@ public class ShapeApp {
         System.out.println("Please enter the length of the rectangle");
         Scanner input = new Scanner(System.in);
         double  length = input.nextDouble();
+        Rectangle shapeRect = new Rectangle();
         System.out.println("You entered: " + length);
+
 
         System.out.println("Please enter the width of the rectangle");
         double  width = input.nextDouble();
